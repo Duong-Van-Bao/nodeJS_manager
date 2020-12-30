9552714734a286ee28abb64a2e6ca0b9dd8817bd
Duong-Van-Bao <DuongBao9109@gmail.com>
2020-12-31 00:31:13 +0700

    code first

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express();
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
+app.use(morgan('combined'))
 
-    var a = 1;
-    var b = 2;
-    var c = a + b;
+app.get('/tin-tuc', (req, res) => {
     return res.send('Hello World!')
 })
 
